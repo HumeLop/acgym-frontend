9ffd23067191717bca8e84c37dd9085792d45baa
HumeLop <loopez321ab@gmail.com>
2026-06-18 11:01:16 -0600

    chore: configure project structure, tooling, and path aliases

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http'
+import {
+  type ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
-};
+    provideZonelessChangeDetection(),
+    provideHttpClient(),
+    provideRouter(routes),
+  ],
+}
